0a06b6d6d033135868ee391ac6114d5a1f89ad55
JuanM-222 <juanmontoya82522@gmail.com>
2023-03-05 21:08:40 -0500

    Creación de la lógica del menú

--- Permutacion,combinacion y variacion/Menu.py
+++ Permutacion,combinacion y variacion/Menu.py
@@ -1,3 +1,5 @@
+import sys
+
 print("""
 ██████╗░██╗███████╗███╗░░██╗██╗░░░██╗███████╗███╗░░██╗██╗██████╗░░█████╗░
 ██╔══██╗██║██╔════╝████╗░██║██║░░░██║██╔════╝████╗░██║██║██╔══██╗██╔══██╗
@@ -16,8 +18,8 @@ def menu():
                             "║ 𝔐𝔢𝔫𝔲́                             ║ \n"+ 
                             "║                                  ║ \n"+
                             "║       Elije una opción           ║ \n"+
-                            "║ 1) variaciones con repeticion    ║ \n"+
-                            "║ 2) Permutacion ordinaria         ║ \n"+
+                            "║ 1) El orden Importa              ║ \n"+
+                            "║ 2) El orden no importa           ║ \n"+
                             "║ 0) Salir                         ║ \n"+
                             "║                                  ║ \n"+
                             "╚══════════════════════════════════╝ \n" ))
@@ -27,14 +29,122 @@ def menu():
             sub_menu2()
         elif opcion_menu == 0:
             print("Fin del Programa....")
-            break 
+            sys.exit()
         else:
             print("Opcion no valida")
         
 
 
 def sub_menu1():
+    while True:
+        opcion_sub_menu1 =  int(input("╔═══════════════════════════════════════╗ \n"+
+                            "║ 𝔐𝔢𝔫𝔲́                                     ║ \n"+ 
+                            "║                                          ║ \n"+
+                            "║         Elije una opción                 ║ \n"+
+                            "║ 1) Todos los elementos intervienen       ║ \n"+
+                            "║ 2) No todos los elementos intervienen    ║ \n"+
+                            "║ 3) Regresar al menú aterior              ║ \n"+
+                            "║ 0) Salir                                 ║ \n"+
+                            "║                                          ║ \n"+
+                            "╚══════════════════════════════════════════╝ \n" ))
+        
+        if opcion_sub_menu1 == 1:
+            while True:
+                opcion_sub_menu1_1 =  int(input("╔═══════════════════════════════════════╗ \n"+
+                                    "║ 𝔐𝔢𝔫𝔲́                                     ║ \n"+ 
+                                    "║                                          ║ \n"+
+                                    "║         Elije una opción                 ║ \n"+
+                                    "║ 1) Los elementos se repiten              ║ \n"+
+                                    "║ 2) Los elementos no se repite            ║ \n"+
+                                    "║ 3) Regresar al menú aterior              ║ \n"+
+                                    "║ 0) Salir                                 ║ \n"+
+                                    "║                                          ║ \n"+
+                                    "╚══════════════════════════════════════════╝ \n" ))
+                
+                if opcion_sub_menu1_1 == 1:
+                    print("Uste esta haciendo una Permutacion con Repeticion ")
+                    return menu()
+                elif opcion_sub_menu1_1 == 2:
+                    print("Usted esta haciendo una Permutacion Ordinaria")
+                    return menu()
+                elif opcion_sub_menu1_1 == 3:
+                    print("Regresando al menú anterior....")
+                    return
+                elif opcion_sub_menu1_1 == 0:
+                    print("Fin del Programa....")
+                    sys.exit()
+                else:
+                    print("Opcion no valida")
+
+        elif opcion_sub_menu1 ==2:
+            while True:
+                opcion_sub_menu1_1 =  int(input("╔═══════════════════════════════════════╗ \n"+
+                                    "║ 𝔐𝔢𝔫𝔲́                                     ║ \n"+ 
+                                    "║                                          ║ \n"+
+                                    "║         Elije una opción                 ║ \n"+
+                                    "║ 1) Los elementos se repiten              ║ \n"+
+                                    "║ 2) Los elementos no se repite            ║ \n"+
+                                    "║ 3) Regresar al menú aterior              ║ \n"+
+                                    "║ 0) Salir                                 ║ \n"+
+                                    "║                                          ║ \n"+
+                                    "╚══════════════════════════════════════════╝ \n" ))
+                
+                if opcion_sub_menu1_1 == 1:
+                    print("Uste esta haciendo una Permutacion con Repeticion ")
+                    return menu()
+                elif opcion_sub_menu1_1 == 2:
+                    print("Usted esta haciendo una Permutacion Ordinaria")
+                    return menu()
+                elif opcion_sub_menu1_1 == 3:
+                    print("Regresando al menú anterior....")
+                    return
+                elif opcion_sub_menu1_1 == 0:
+                    print("Fin del Programa....")
+                    sys.exit()
+                else:
+                    print("Opcion no valida")
+            
+        
+
+        elif opcion_sub_menu1 == 3:
+            print("Regresando al menú anterior....")
+            return
+        elif opcion_sub_menu1 == 0:
+            print("Fin del Programa....")
+            sys.exit()
+        else:
+            print("Opcion no valida")
+
+
 
 
 def sub_menu2():
+    while True:
+        opcion_sub_menu2 =  int(input("╔══════════════════════════════════════════╗ \n"+
+                            "║ 𝔐𝔢𝔫𝔲́                                     ║ \n"+ 
+                            "║                                          ║ \n"+
+                            "║         Elije una opción                 ║ \n"+
+                            "║ 1) Los elementos se repiten              ║ \n"+
+                            "║ 2) Los elementos no se repite            ║ \n"+
+                            "║ 3) Regresar al menú aterior              ║ \n"+
+                            "║ 0) Salir                                 ║ \n"+
+                            "║                                          ║ \n"+
+                            "╚══════════════════════════════════════════╝ \n" ))
+        
+        if opcion_sub_menu2 == 1:
+            print("Uste esta haciendo una Combinacion con Repeticion ")
+            return menu()
+        elif opcion_sub_menu2 == 2:
+            print("Usted esta haciendo una Combinacion Ordinaria")
+            return menu()
+        elif opcion_sub_menu2 == 3:
+            print("Regresando al menú anterior....")
+            return
+        elif opcion_sub_menu2 == 0:
+            print("Fin del Programa....")
+            sys.exit()
+        else:
+            print("Opcion no valida")
+    
 
+menu()
